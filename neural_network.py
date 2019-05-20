138729e05dd2b9bc8838e7f37a4905600183ffce
rodrigolopezag <rodrigolopezag@gmail.com>
2019-05-20 12:57:43 -0700

    Added precision, recall and confusion matrix for accuracy measurements

--- neural_network.py
+++ neural_network.py
@@ -5,11 +5,14 @@ import nltk
 import os
 import json
 import datetime
+import itertools
 import pandas as pd
 import numpy as np
 import time
 
 from nltk.stem.lancaster import LancasterStemmer
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
 
 
 class Neural_Network():
@@ -38,10 +41,105 @@ class Neural_Network():
             self.synapse = json.load(data_file) 
             self.synapse_0 = np.asarray(self.synapse['synapse0']) 
             self.synapse_1 = np.asarray(self.synapse['synapse1'])
+    
+    # Helpful functions to calculate the precision and recall of our neural network
+    def precision(self, label, confusion_matrix):
+        col = confusion_matrix[:, label]
+        return confusion_matrix[label, label] / col.sum()
+    
+    def recall(self, label, confusion_matrix):
+        row = confusion_matrix[label, :]
+        return confusion_matrix[label, label] / row.sum()
+
+    def precision_macro_average(self, confusion_matrix):
+        rows, columns = confusion_matrix.shape
+        sum_of_precisions = 0
+        for label in range(rows):
+            sum_of_precisions += precision(label, confusion_matrix)
+        return sum_of_precisions / rows
+
+    def recall_macro_average(self, confusion_matrix):
+        rows, columns = confusion_matrix.shape
+        sum_of_recalls = 0
+        for label in range(columns):
+            sum_of_recalls += recall(label, confusion_matrix)
+        return sum_of_recalls / columns
+    
+    def accuracy(self, confusion_matrix):
+        diagonal_sum = confusion_matrix.trace()
+        sum_of_all_elements = confusion_matrix.sum()
+        return diagonal_sum / sum_of_all_elements
+    
+    #########################################################################
+    
+    def confusion_matrix_setup(self, csv_test):
+        # y_target =    [1, 1, 1, 0, 0, 2, 0, 3]
+        # y_predicted = [1, 0, 1, 0, 0, 2, 1, 3]
+        y_target = list()
+        y_predicted = list()
+
+        test = pd.read_csv(csv_test)
+
+        #Setup y_target first
+        for row in test.itertuples():
+            y_target.append(row.category_id)
+        
+        for row in test.itertuples():
+            category = self.classify(row.command)[0][0]
+            if category == 'Streaming':
+                y_predicted.append(0)
+            elif category == 'TV':
+                y_predicted.append(1)
+            else:
+                y_predicted.append(2)
+        # print(y_predicted)
+        self.cnf_matrix = confusion_matrix(y_target, y_predicted)
+        print(self.cnf_matrix)
+        plt.figure()
+        self.plot_confusion_matrix(self.cnf_matrix, classes=['Streaming', 'TV', 'Pre-programacion'], normalize=True, title='Confusion matrix, normalization')
+    
+    def plot_confusion_matrix(self, cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
+        # This function prints and plots the confusion matrix.
+        # Normalization can be applied by setting "normalize=True".
+        
+        if normalize:
+            cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+            print("Normalized confusion matrix")
+        else:
+            print('Confusion matrix, without normalization')
+
+        print(cm)
+
+        plt.imshow(cm, interpolation='nearest', cmap=cmap)
+        plt.title(title)
+        plt.colorbar()
+        tick_marks = np.arange(len(classes))
+        plt.xticks(tick_marks, classes, rotation=45)
+        plt.yticks(tick_marks, classes)
+
+        fmt = '.2f' if normalize else 'd'
+        thresh = cm.max() / 2.
+        for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+            plt.text(j, i, format(cm[i, j], fmt),
+                    horizontalalignment="center",
+                    color="white" if cm[i, j] > thresh else "black")
+
+        plt.ylabel('True label')
+        plt.xlabel('Predicted label')
+        plt.tight_layout()
+        plt.show()
+
 
     def pre_process_data(self):
+        # Have the data in a specific format for the subsequently training
         self.training_data = []
+        counter = 0
+        k_fold_limit = 69 * 9
         for row in self.df.itertuples():
+            if counter == k_fold_limit:
+                break
+            else:
+                counter += 1
             if row.category_id == 0:
                 self.training_data.append({"class":row.category, "sentence":row.commands})
             elif row.category_id == 1:
@@ -50,6 +148,7 @@ class Neural_Network():
                 self.training_data.append({"class":row.category, "sentence":row.commands})
     
     def stem(self):
+        # Get the stem of the most used words of our training data and bag of words
         self.words = []
         self.classes = []
         self.documents = []
@@ -98,7 +197,7 @@ class Neural_Network():
             self.output.append(self.output_row)
     
     
-    # Helpful functions
+    # Helpful functions for the neural network calculations
     # compute sigmoid nonlinearity
     def sigmoid(self, x):
         output = 1/(1+np.exp(-x))
@@ -141,6 +240,8 @@ class Neural_Network():
         # output layer
         l2 = self.sigmoid(np.dot(l1, self.synapse_1))
         return l2
+    
+    ###########################################################################
 
     # Train Neural Model
     def train(self, X, y, hidden_neurons=10, alpha=1, epochs=50000, dropout=False, dropout_percent=0.5):
@@ -227,15 +328,31 @@ class Neural_Network():
         results = [[i,r] for i,r in enumerate(results) if r>self.ERROR_THRESHOLD ] 
         results.sort(key=lambda x: x[1], reverse=True) 
         return_results =[[self.classes[r[0]],r[1]] for r in results]
-        print ("%s \n classification: %s" % (sentence, return_results))
+        # print ("%s \n classification: %s" % (sentence, return_results))
         return return_results
 
+    
+
+
+# Making the neural network
 neural = Neural_Network('ConjuntoDatos-Completo.csv')
-neural.classify("esta activado el control parental?")
-neural.classify("mete esa pelicula a mis favoritos")
-neural.classify("cambiale a la temporada dos")
-neural.classify("activa el menu de la programacion")
-neural.classify("cuando tengo que pagar netflix?")
-neural.classify("activa la consola de videojuegos")
+
+# Test cases ###########################################
+# neural.classify("esta activado el control parental?")
+# neural.classify("mete esa pelicula a mis favoritos")
+# neural.classify("cambiale a la temporada dos")
+# neural.classify("activa el menu de la programacion")
+# neural.classify("cuando tengo que pagar netflix?")
+# neural.classify("activa la consola de videojuegos")
 # print()
-# classify("colocame un recordatorio para Lucifer a las nueve de la noche", show_details=True)
+# classify("colocame un recordatorio para Lucifer a las nueve de la noche", show_details=True) # For more specificity
+#######################################################
+
+# Testing the accuracy, precision and recall for several test cases excluded from the training data
+neural.confusion_matrix_setup("Conjunto-de-Validación-Mini.csv")
+print("label precision recall")
+for label in range(3):
+    print(f"{label:5d} {neural.precision(label, neural.cnf_matrix):9.3f} {neural.recall(label, neural.cnf_matrix):6.3f}")
+
+print('Accuracy')
+print(neural.accuracy(neural.cnf_matrix))
